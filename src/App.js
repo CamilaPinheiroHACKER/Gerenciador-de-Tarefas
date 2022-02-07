458629b56d9df348c9891365b8d780111cd6f480
camilapinheiro <camila.eug.pinheiro@gmail.com>
2022-02-07 18:51:23 -0300

    Ciração de uma api de tarefas em react com funções de adicionar tarefas, excluir tarefas e dar um ok na tarefa concluida

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import "./App.css";
-
-const App = () => {
-    const message = 'Hello world!'
-    return <h1>{message}</h1>
-};
-
-export default App;
---
+++ src/components/AddTask.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Button from "./Button";
+import "./Tasks.css";
+
+const AddTask = ({ handleTaskAddition }) => {
+	const [inputData, setInputData] = useState("");
+
+	const handleInputChange = (e) => {
+		setInputData(e.target.value);
+	};
+
+	const handleAddTaskClick = () => {
+		handleTaskAddition(inputData);
+		setInputData("");
+	};
+
+	return (
+		<div className="add-task-container">
+			<input
+				onChange={handleInputChange}
+				value={inputData}
+				className="add-task-input"
+				type="text"
+			/>
+			;
+			<div className="add-button-container">
+				<Button onClick={handleAddTaskClick}>Adicionar</Button>
+			</div>
+		</div>
+	);
+};
+
+export default AddTask;
